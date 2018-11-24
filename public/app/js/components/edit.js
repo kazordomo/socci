068008230099c17a22955c70d8c659f3ba8f4341
Zak Eklund <zak.eklund@live.se>
2018-11-24 10:36:45 +0100

    progress on edit view

--- public/app/js/components/edit.js
+++ public/app/js/components/edit.js
@@ -3,7 +3,33 @@
 class Edit {
 
     constructor() {
+        this.DOMElement = document.querySelector('section#edit');
 
+        this.init();
+        
+    }
+
+    init () {
+        this.addAttendee();
+    }
+
+    addAttendee () {
+
+        let buttonEl = this.DOMElement.querySelector('form button');
+        let newAttendeeEl = this.DOMElement.querySelector('input[name="attendee"]');
+        let attendeesEl = this.DOMElement.querySelector('.attendees');
+
+        buttonEl.addEventListener('click', event => {
+
+            event.preventDefault();
+
+            attendeesEl.innerHTML += `, ${newAttendeeEl.value}`;
+            
+            newAttendeeEl.value = '';
+
+        });
+
+        return true;
     }
 
 }
