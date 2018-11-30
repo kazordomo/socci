e444e8809ba3321c38f3560dc1cbecd4e1195486
Zak Eklund <zak.eklund@live.se>
2018-11-30 21:57:20 +0100

    refactored the createActivityEl function

--- public/app/js/components/home.js
+++ public/app/js/components/home.js
@@ -32,87 +32,55 @@ class Home {
         
     }
 
-    // TODO: REFACTOR!! Use elements/activityRow?
     createActivityEl (data) {
 
+        let htmlString = `
+            <div class="top">
+                <h2>${data.title}</h2>
+                <span>${data.time}</span>
+            </div>
+            <div class="container">
+                <div>
+                    ${data.attendees.map(attendee => `<span>${attendee.name}</span>`)}
+                </div>
+                <button class="button success">Attend</button>
+                <button class="button danger">Delete</button>
+            </div>
+        `;
+
         let rowEl = document.createElement('div');
-        let attendeesEl = document.createElement('div');
-        let headerEl = document.createElement('div');
-        let containerEl = document.createElement('div');
-        let titleEl = document.createElement('h2');
-        let timeEl = document.createElement('span');
-        let attendEl = this.createAttendButton(data._id, rowEl);
-        let deleteEl = this.createDeleteButton(data._id, rowEl);
+            rowEl.classList.add('row');
+            rowEl.innerHTML = htmlString;
 
-        rowEl.className = 'row out';
-        headerEl.className = 'top';
-        containerEl.className = 'container';
-
-        titleEl.innerHTML = data.title;
-        timeEl.innerHTML = data.time;
-
-        headerEl.appendChild(titleEl);
-        headerEl.appendChild(timeEl);
-
-        // Wrap each attendee in a span and append to div
-        for (let attendee of data.attendees) {
-
-            let span = document.createElement('span');
-            span.innerHTML = attendee.name;
-            attendeesEl.appendChild(span);
-            
-        }
-
-        containerEl.appendChild(attendeesEl);
-        containerEl.appendChild(attendEl);
-        containerEl.appendChild(deleteEl);
-        rowEl.appendChild(headerEl);
-        rowEl.appendChild(containerEl);
-
-        headerEl.addEventListener('click', () => rowEl.classList.toggle('active'));
+        rowEl.querySelector('.top')
+             .addEventListener('click', () => rowEl.classList.toggle('active'));
+        rowEl.querySelector('button.success')
+             .addEventListener('click', () => this.onAttend(data._id, rowEl));
+        rowEl.querySelector('button.danger')
+             .addEventListener('click', () => this.onDelete(data._id, rowEl));
 
         this.DOMElement.querySelector('.wrapper').appendChild(rowEl);
         
         return true;
     }
 
-    createAttendButton (id, rowEl) {
-        let element = document.createElement('button');
+    async onAttend (id, rowEl) {
 
-        element.className = 'button success';
-        element.innerHTML = 'Attend';
+        rowEl.classList.toggle('attending');
 
-        element.addEventListener('click', async () => {
+        if(!rowEl.classList.contains('attending')) {
+            await ActivityCtrl.attendActivity({id: 999, name: 'Dummyname'}, id);
+        } else {
+            await ActivityCtrl.declineActivity({id: 999, name: 'Dummyname'}, id);
+        }
 
-            rowEl.classList.toggle('attending');
-
-            if(!rowEl.classList.contains('attending')) {
-                await ActivityCtrl.attendActivity({id: 999, name: 'Dummyname'}, id);
-                element.innerHTML = 'Attend';
-            } else {
-                await ActivityCtrl.declineActivity({id: 999, name: 'Dummyname'}, id);
-                element.innerHTML = 'Do Not Attend';
-            }
-
-        });
-
-        return element;
     }
 
-    createDeleteButton (id, rowEl) {
-        let element = document.createElement('button');
+    async onDelete (id, rowEl) {
 
-        element.className = 'button danger';
-        element.innerHTML = 'Delete';
+        this.DOMElement.querySelector('.wrapper').removeChild(rowEl);
+        ActivityCtrl.deleteActivity(id);
 
-        element.addEventListener('click', () => {
-
-            this.DOMElement.querySelector('.wrapper').removeChild(rowEl);
-            ActivityCtrl.deleteActivity(id);
-
-        });
-
-        return element;
     }
 
 }
