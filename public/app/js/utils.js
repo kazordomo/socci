63efa1b9ace3009f6f90cf050514bb0086818d3f
Zak Eklund <zak.eklund@live.se>
2018-12-02 15:51:30 +0100

    added comments

--- public/app/js/utils.js
+++ public/app/js/utils.js
@@ -1,15 +1,22 @@
 class Utils {
 
     static domWithData (DOMElement, data) {
+
+        // Get the DOM as a tring.
         let html = DOMElement.innerHTML;
 
+        // Regex rule.
         let regExp = /\{{([^}}]+)/g;
+        // Get all values surrounded by {{ }}.
         let matches = html.match(regExp);
 
         for(let match of matches) {
+            // Get the property. {{ data.test }} -> test
             let prop = match.split('.')[1].trim();
             if (data[prop]) {
+                // Add back the }} to be able to match in the html.
                 match = match + '}}';
+                // Replace the match with the correct propert from data.
                 html = html.replace(match, data[prop]);
             }
         }
