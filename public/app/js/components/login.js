6b249e5e74d7a1b717ee902dd9dc986e82cec4e5
Zak Eklund <zak.eklund@live.se>
2018-12-04 19:00:06 +0100

    random styling

--- public/app/js/components/login.js
+++ public/app/js/components/login.js
@@ -18,60 +18,59 @@ class Login {
         let changeAuthTypeEl = this.DOMElement.querySelectorAll('a[data-type]');
 
         this.DOMElement.querySelector('button[data-type="login"]').addEventListener('click', () => {
-
-            let userData = {
-                email: this.DOMElement.querySelector('input[name="email"]').value,
-                password: this.DOMElement.querySelector('input[name="password"]').value,
-            }
-
-            AuthCtrl.login(userData);
-
-            return true;
-
+            this.onLogin();
         });
 
         this.DOMElement.querySelector('button[data-type="register"]').addEventListener('click', () => {
-
-            let userData = {
-                email: this.DOMElement.querySelector('input[name="email"]').value,
-                password: this.DOMElement.querySelector('input[name="password"]').value,
-                retype_password: this.DOMElement.querySelector('input[name="retype_password"]').value,
-            }
-
-            AuthCtrl.register(userData);
-
-            return true;
-
+            this.onRegister();
         });
 
         // Change to Register inputs
         changeAuthTypeEl[0].addEventListener('click', () => { 
-
-            for(let element of this.registerElements) {
-                element.classList.remove('inactive');
-            }
-
-            for(let element of this.loginElements) {
-                element.classList.add('inactive');
-            }
-
-            return true;
+            this.changeAuthType(this.registerElements, this.loginElements);
         });
 
         // CHange to Login inputs
         changeAuthTypeEl[1].addEventListener('click', () => { 
-
-            for(let element of this.registerElements) {
-                element.classList.add('inactive');
-            }
-
-            for(let element of this.loginElements) {
-                element.classList.remove('inactive');
-            }
-
-            return true;
+            this.changeAuthType(this.loginElements, this.registerElements);
         });
 
+        return true;
+    }
+
+    onLogin () {
+        let userData = {
+            email: this.DOMElement.querySelector('input[name="email"]').value,
+            password: this.DOMElement.querySelector('input[name="password"]').value,
+        }
+
+        AuthCtrl.login(userData);
+
+        return true;
+    }
+
+    onRegister () {
+        let userData = {
+            email: this.DOMElement.querySelector('input[name="email"]').value,
+            password: this.DOMElement.querySelector('input[name="password"]').value,
+            retype_password: this.DOMElement.querySelector('input[name="retype_password"]').value,
+        }
+
+        AuthCtrl.register(userData);
+
+        return true;
+    }
+
+    changeAuthType (activeAlements, inactiveElements) {
+        for(let element of activeAlements) {
+            element.classList.remove('inactive');
+        }
+
+        for(let element of inactiveElements) {
+            element.classList.add('inactive');
+        }
+
+        return true;
     }
 
 }
